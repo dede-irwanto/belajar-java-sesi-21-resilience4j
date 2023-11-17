cb361bee88a7fbcee0f91b4968a9e4fe463d7ef3
unknown <id.dedeirwanto@gmail.com>
2023-11-17 13:52:31 +0800

    first commit

---
+++ src/main/java/programmerzamannow/resilience4j/BelajarJavaSesi21Resilience4jApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaSesi21Resilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaSesi21Resilience4jApplication.class, args);
+	}
+
+}
